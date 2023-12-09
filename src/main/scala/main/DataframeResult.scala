d8355b0a926e4940ce59c107c3ff9d8a23dc352c
Moundji25 <moundjibel@hotmail.com>
2023-12-09 11:04:33 +0100

    arrivée à la requete 20 reste quelques un

--- src/main/scala/main/DataframeResult.scala
+++ src/main/scala/main/DataframeResult.scala
@@ -2,6 +2,8 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.Window
+
 
 case class DataFrameResult(val spark : SparkSession)
 {
@@ -17,9 +19,6 @@ case class DataFrameResult(val spark : SparkSession)
     }
     def Requete_3 (df_output_full :DataFrame,df_country_classes :DataFrame ): DataFrame = 
     {
-        //val tmp_df = df_country_classes.withColumnRenamed("country_code","country_code_1")
-        //tmp_df.cache()
-
         //country_code,country_label
         df_output_full.join(
             df_country_classes.withColumnRenamed("country_code","country_code_1"),
@@ -39,36 +38,163 @@ case class DataFrameResult(val spark : SparkSession)
     def Requete_5 (df_output_full :DataFrame,df_goods_classes :DataFrame): DataFrame = 
     {
          df_output_full.filter(col("product_type") === "Goods").join(
-            df_goods_classes.withColumnRenamed("NZHSC_Level_1_Code_HS2","code_1"),
+            df_goods_classes.withColumnRenamed("NZHSC_Level_2_Code_HS4","code_1"),
             col("code") === col("code_1"),
             "left_outer"
-            ).withColumnRenamed("NZHSC_Level_1","details_goods").drop("code_1").drop("NZHSC_Level_2").drop("NZHSC_Level_2_Code_HS4")
+            ).withColumnRenamed("NZHSC_Level_1","details_goods").drop("code_1").drop("NZHSC_Level_2").drop("NZHSC_Level_1_Code_HS2")
     }
     def Requete_6 (df :DataFrame): Map[String,DataFrame] = 
     {
-        window_spec = Window.orderBy(col("count").desc)
+        val window_spec = Window.orderBy(col("count").desc)
 
-        var tmp_grouped = df.filter(col("product_type") === "Goods").filter(col("account") === "Exports")\
-        .groupBy("country_code").count()
-
-        // Classer les données en fonction du nombre d'occurences
-        val rank_goods = tmp_grouped.withColumn("rank", rank().over(window_spec)).drop(col("rank"))
+        val tmp_grouped = df.filter(col("product_type") === "Goods").filter(col("account") === "Exports").groupBy("country_code").count()
+        val rank_goods = tmp_grouped.withColumn("rank", rank().over(window_spec)).drop(col("count"))
         
-        tmp_grouped = df.filter(col("product_type") === "Service").filter(col("account") === "Exports")\
-        .groupBy("country_code").count()
-
-        val rank_services= tmp_grouped.withColumn("rank", rank().over(window_spec)).drop(col("rank"))
+        
+        val tmp_grouped_2 = df.filter(col("product_type") === "Services").filter(col("account") === "Exports").groupBy("country_code").count()
+        val rank_services= tmp_grouped_2.withColumn("rank", rank().over(window_spec)).drop(col("count"))
 
         return Map("goods" -> rank_goods, "services" -> rank_services)
-
-
     }
-    /*def Requete_7 (df :DataFrame): DataFrame = 
+    def Requete_7 (df :DataFrame): Map[String,DataFrame] = 
     {
+        val window_spec = Window.orderBy(col("count").desc)
 
+        val tmp_grouped = df.filter(col("product_type") === "Goods").filter(col("account") === "Imports").groupBy("country_code").count()
+        val rank_goods = tmp_grouped.withColumn("rank", rank().over(window_spec)).drop(col("count"))
+        
+        val tmp_grouped_2 = df.filter(col("product_type") === "Services").filter(col("account") === "Imports").groupBy("country_code").count()
+        val rank_services= tmp_grouped_2.withColumn("rank", rank().over(window_spec)).drop(col("count"))
+
+        return Map("goods" -> rank_goods, "services" -> rank_services)
     }
     def Requete_8 (df :DataFrame): DataFrame = 
     {
+        df.filter(col("product_type") === "Goods" ).groupBy(col("product_type")).count()
+    }
+    def Requete_9 (df :DataFrame): DataFrame = 
+    {
+        df.filter(col("product_type") === "Services" ).groupBy(col("product_type")).count()
+    }
+    def Requete_10 (df :DataFrame): DataFrame = 
+    {
+        df.where((col("account") === "Exports") and (col("country_code") === "FR") and (col("product_type") === "Goods" ))
+    }
+    def Requete_11 (df :DataFrame): DataFrame = 
+    {
+        df.where((col("account") === "Imports") and (col("country_code") === "FR")  (col("product_type") === "Services" ))
+    }
+    def Requete_12 (df :DataFrame, df_services : DataFrame): DataFrame = 
+    {
+        // SOLUTION WINDOW MARCHE :)
+        //val window_spec = Window.partitionBy(col("service_label"))
 
-    }*/
+        //df.filter(col("product_type") === "Services").join(df_services.withColumnRenamed("code","code_1"),col("code") === col("code_1"),"left_outer").withColumn(
+        //    "nbre_occurence", count("*").over(window_spec)).orderBy(col("nbre_occurence")).select("service_label","nbre_occurence").distinct()
+        val window_spec = Window.orderBy(("nbre_occurences"))
+
+        df_services.join(df.filter(col("product_type") === "Services").withColumnRenamed("code","code_1"), col("code") === col("code_1"),"left_outer").groupBy(
+            col("code_1"),col("service_label")).count().withColumnRenamed("count","nbre_occurences").withColumn("rang",rank().over(window_spec))
+    }
+    def Requete_13 (df :DataFrame, df_goods : DataFrame): DataFrame = 
+    {
+        // SOLUTION WINDOW MARCHE :)
+        //val window_spec = Window.partitionBy(col("NZHSC_Level_2_Code_HS4"),col("NZHSC_Level_1_Code_HS2"))
+
+        //df.filter(col("product_type") === "Goods").join(df_goods,( col("code") === col("NZHSC_Level_2_Code_HS4") || col("code") === col("NZHSC_Level_1_Code_HS2")),"left_outer").withColumn(
+        //    "nbre_occurence", count("*").over(window_spec)).orderBy(desc("nbre_occurence")).select("NZHSC_Level_2","NZHSC_Level_1","nbre_occurence").distinct()
+
+        val window_spec = Window.orderBy(desc("nbre_occurences"))
+
+        df_goods.join(df.filter(col("product_type") === "Goods"), col("code") === col("NZHSC_Level_2_Code_HS4") || col("code") === col("NZHSC_Level_1_Code_HS2"),"left_outer").groupBy(
+            col("NZHSC_Level_2_Code_HS4"),col("NZHSC_Level_1_Code_HS2"),col("NZHSC_Level_1")).count().withColumnRenamed("count","nbre_occurences").withColumn("rang",rank().over(window_spec))
+    }
+    def Requete_14 (df :DataFrame, df_countries: DataFrame): DataFrame = 
+    {
+        // SOLUTION WINDOW A REVOIR !!! 
+        //val window_spec = Window.partitionBy(col("country_code"))
+
+        //val df_imports = df.filter(col("account") === "Imports").withColumn("status_import", count("*").over(window_spec)).withColumnRenamed("code","code_1")
+
+        //val tmp_df = df.filter(col("account") === "Exports").withColumn("status_export", count("*").over(window_spec)).withColumnRenamed("country_code","country_code_1").join(
+        //    df_imports,col("code") === col("code_1"),"left_outer"
+        //).select("status_import","status_export","country_code_1")
+
+        /*df_countries.join( tmp_df,
+            col("country_code") === col("country_code_1"),
+            "left_outer"
+        ).drop("country_code_1").withColumn("status_import_export", when(col("status_import") > col("status_export"), lit("N")).otherwise(lit("P"))).select(
+            "country_code","country_label","status_import_export"
+        ).distinct()*/
+
+        // SOLUTION GROUP BY 
+        val df_imports = df.filter(col("account") === "Imports").groupBy(col("country_code")).sum("value").withColumnRenamed("sum(value)","status_import").withColumnRenamed("country_code","country_code_1")
+        val df_exports = df.filter(col("account") === "Exports").groupBy(col("country_code")).sum("value").withColumnRenamed("sum(value)","status_export").withColumnRenamed("country_code","country_code_1")
+        df_countries.join(df_exports,col("country_code") === col("country_code_1") ,"left_outer").drop(col("country_code_1")).join(df_imports,col("country_code") === col("country_code_1"),"left_outer").withColumn(
+            "status_import_export", when(col("status_import") > col("status_export"), lit("N")).otherwise(lit("P"))).select("country_code","country_label","status_import_export")
+    }
+    def Requete_15 (df :DataFrame, df_countries: DataFrame): DataFrame = 
+    {
+        val df_imports = df.filter(col("account") === "Imports").groupBy(col("country_code")).sum("value").withColumnRenamed("sum(value)","status_import").withColumnRenamed("country_code","country_code_1")
+        val df_exports = df.filter(col("account") === "Exports").groupBy(col("country_code")).sum("value").withColumnRenamed("sum(value)","status_export").withColumnRenamed("country_code","country_code_1")
+        df_countries.join(df_exports,col("country_code") === col("country_code_1") ,"left_outer").drop(col("country_code_1")).join(df_imports,col("country_code") === col("country_code_1"),"left_outer").withColumn(
+            "difference_import_export", col("status_export") - col("status_import") ).select("country_code","country_label","difference_import_export")
+    }
+    def Requete_16 (df :DataFrame): DataFrame = 
+    {
+       df.filter(col("product_type") === "Goods").groupBy(col("country_code")).sum("value").withColumnRenamed("sum(value)","somme_good")
+    }
+    def Requete_17 (df :DataFrame): DataFrame = 
+    {
+        df.filter(col("product_type") === "Services").groupBy(col("country_code")).sum("value").withColumnRenamed("sum(value)","somme_service")
+    }
+    def Requete_18 (df :DataFrame): DataFrame = 
+    {
+        // faire une colonne sum pour chaque pays via une window 
+        // faire un groupBy faire sum > faire collect > récup la valeur des 100% 
+        val somme_goods_imports = df.filter(col("product_type") === "Goods").filter(col("account") === "Imports").groupBy(col("product_type")).sum("value").drop("product_type").head().get(0)
+        val somme_goods_exports = df.filter(col("product_type") === "Goods").filter(col("account") === "Exports").groupBy(col("product_type")).sum("value").drop("product_type").head().get(0)
+        
+        // faire un window pour mettre pour chaque ligne la somme pour le pays de la ligne (pour l'export et l'import)
+        // faire deux colonne : un pour import et un autre pour export > faire deux colonnes de prctg ou on divise 
+        df.filter(col("product_type") === "Services").groupBy(col("country_code")).sum("value").withColumnRenamed("sum(value)","somme_service")
+    }
+    def Requete_19 (df :DataFrame): Map[String,DataFrame]  = 
+    {
+       val window_spec = Window.partitionBy(col("product_type"))
+       val window_spec_2 = Window.partitionBy(col("country_code"))
+
+       val df_goods_prctg_imports = df.filter(col("product_type") === "Goods").filter(col("account") === "Imports").withColumn("nbre_totale_goods", count("*").over(window_spec)).withColumn(
+        "nbre_goods_pays",count("*").over(window_spec_2)
+       ).withColumn("prctg_goods",col("nbre_goods_pays") / col("nbre_totale_goods")).groupBy(col("country_code"),col("product_type"),col("prctg_goods")).count().drop("count")
+
+
+       val df_goods_prctg_exports = df.filter(col("product_type") === "Goods").filter(col("account") === "Exports").withColumn("nbre_totale_goods", count("*").over(window_spec)).withColumn(
+        "nbre_goods_pays",count("*").over(window_spec_2)
+       ).withColumn("prctg_goods",col("nbre_goods_pays") / col("nbre_totale_goods")).drop("nbre_totale_goods","nbre_goods_pays").groupBy(col("country_code"),col("product_type"),col("prctg_goods")).count().drop("count")
+
+       return Map("imports_prctg_goods" -> df_goods_prctg_imports, "exports_prctg_goods" -> df_goods_prctg_exports)
+    }
+
+    def Requete_20 (df :DataFrame): Map[String,DataFrame]  = 
+    {
+       val window_spec = Window.partitionBy(col("product_type"))
+       val window_spec_2 = Window.partitionBy(col("country_code"))
+
+       val df_goods_prctg_imports = df.filter(col("product_type") === "Services").filter(col("account") === "Imports").withColumn("nbre_totale_services", count("*").over(window_spec)).withColumn(
+        "nbre_services_pays",count("*").over(window_spec_2)
+       ).withColumn("prctg_services",col("nbre_services_pays") / col("nbre_totale_services")).groupBy(col("country_code"),col("product_type"),col("prctg_services")).count().drop("count")
+
+
+       val df_goods_prctg_exports = df.filter(col("product_type") === "Services").filter(col("account") === "Exports").withColumn("nbre_totale_services", count("*").over(window_spec)).withColumn(
+        "nbre_services_pays",count("*").over(window_spec_2)
+       ).withColumn("prctg_services",col("nbre_services_pays") / col("nbre_totale_services")).drop("nbre_totale_services","nbre_services_pays").groupBy(col("country_code"),col("product_type"),col("prctg_services")).count().drop("count")
+
+       return Map("imports_prctg_services" -> df_goods_prctg_imports, "exports_prctg_services" -> df_goods_prctg_exports)
+    }
+
+    def Requete_21 (df :DataFrame): DataFrame = 
+    {
+       df.filter(col("product_type") === "Goods").withColumn("code_HS2", when(len(col("code")) > 2,substr(col("code"),0,2), col("code")) ).groupBy(col("code_HS2")).count()
+    }
 }
--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -61,14 +61,151 @@ object Hello
     println("REQUETE 04 ")
     df_requete_4.show()
     df_requete_4.printSchema()
-  */ 
+  
     val df_requete_5 = DataFrameResult.Requete_5(output_full,goods_classes)
     df_requete_5.cache()
 
     println("REQUETE 05 ")
-    //df_requete_5.show()
-    //df_requete_5.printSchema()
+    df_requete_5.show()
+    df_requete_5.printSchema()
 
-  }
+   
+  val tmp_map = DataFrameResult.Requete_6(output_full)
+  val df_requete_6_goods = tmp_map("goods")
+  val df_requete_6_services = tmp_map("services")
 
+  df_requete_6_goods.cache()
+  df_requete_6_services.cache()
+
+  println("REQUETE 06 ")
+  println(" les pays exporatateurs par goods ")
+  df_requete_6_goods.show()
+  df_requete_6_goods.printSchema()
+  println(" les pays exporatateurs par services ")
+  df_requete_6_services.show()
+  df_requete_6_services.printSchema()
+
+  val tmp_map = DataFrameResult.Requete_7(output_full)
+  val df_requete_7_goods = tmp_map("goods")
+  val df_requete_7_services = tmp_map("services")
+
+  df_requete_7_goods.cache()
+  df_requete_7_services.cache()
+
+  println("REQUETE 07 ")
+  println(" les pays imporatateurs par goods ")
+  df_requete_7_goods.show()
+  df_requete_7_goods.printSchema()
+  println(" les pays imporatateurs par services ")
+  df_requete_7_services.show()
+  df_requete_7_services.printSchema()
+
+val df_requete_8 = DataFrameResult.Requete_8(output_full)
+    //df_requete_8.cache()
+
+    println("REQUETE 08 ")
+    df_requete_8.show()
+    df_requete_8.printSchema()
+
+val df_requete_9 = DataFrameResult.Requete_8(output_full)
+    //df_requete_9.cache()
+
+    println("REQUETE 09 ")
+    df_requete_9.show()
+    df_requete_9.printSchema()
+
+val df_requete_10 = DataFrameResult.Requete_8(output_full)
+    //df_requete_10.cache()
+
+    println("REQUETE 10 ")
+    df_requete_10.show()
+    df_requete_10.printSchema()
+
+val df_requete_11 = DataFrameResult.Requete_11(output_full)
+    //df_requete_11.cache()
+
+    println("REQUETE 11 ")
+    df_requete_11.show()
+    df_requete_11.printSchema()
+
+
+val df_requete_12 = DataFrameResult.Requete_12(output_full,services_classes)
+    df_requete_12.cache()
+
+    println("REQUETE 12 ")
+    df_requete_12.show()
+    df_requete_12.printSchema()
+
+val df_requete_13 = DataFrameResult.Requete_13(output_full,goods_classes)
+    df_requete_13.cache()
+
+    println("REQUETE 13 ")
+    df_requete_13.show()
+    df_requete_13.printSchema()
+
+val df_requete_14 = DataFrameResult.Requete_14(output_full,country_classes)
+    df_requete_14.cache()
+
+    println("REQUETE 14 ")
+    df_requete_14.show()
+    //df_requete_14.printSchema()
+
+val df_requete_15 = DataFrameResult.Requete_15(output_full,country_classes)
+    //df_requete_15.cache()
+
+    println("REQUETE 15 ")
+    df_requete_15.show()
+    df_requete_15.printSchema()
+
+val df_requete_16 = DataFrameResult.Requete_16(output_full)
+    //df_requete_16.cache()
+
+    println("REQUETE 16 ")
+    df_requete_16.show()
+    df_requete_16.printSchema()
+
+val df_requete_17 = DataFrameResult.Requete_17(output_full)
+    //df_requete_17.cache()
+
+    println("REQUETE 17 ")
+    df_requete_17.show()
+    df_requete_17.printSchema()
+    
+val tmp_map = DataFrameResult.Requete_19(output_full)
+    //df_requete_19.cache()
+
+val df_requete_19_imports_prctg_goods = tmp_map("imports_prctg_goods")
+val df_requete_19_exports_prctg_goods = tmp_map("exports_prctg_goods")
+
+  //df_requete_19_exports_prctg_goods.cache()
+  //df_requete_19_imports_prctg_goods.cache()
+
+  println("REQUETE 19 ")
+  println(" le pourcentage de goods pour les imports par pays : ")
+  df_requete_19_imports_prctg_goods.show()
+  df_requete_19_imports_prctg_goods.printSchema()
+  println(" le pourcentage de goods pour les exports par pays : ")
+  df_requete_19_exports_prctg_goods.show()
+  df_requete_19_exports_prctg_goods.printSchema()
+*/
+  val tmp_map = DataFrameResult.Requete_20(output_full)
+    //df_requete_20.cache()
+
+val df_requete_20_imports_prctg_services = tmp_map("imports_prctg_services")
+val df_requete_20_exports_prctg_services = tmp_map("exports_prctg_services")
+
+  //df_requete_20_exports_prctg_goods.cache()
+  //df_requete_20_imports_prctg_goods.cache()
+
+  println("REQUETE 20 ")
+  println(" le pourcentage de goods pour les imports par pays : ")
+  df_requete_20_imports_prctg_services.show()
+  df_requete_20_imports_prctg_services.printSchema()
+  println(" le pourcentage de goods pour les exports par pays : ")
+  df_requete_20_exports_prctg_services.show()
+  df_requete_20_exports_prctg_services.printSchema()
+
+
+
+}
 }
